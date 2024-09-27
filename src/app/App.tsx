32275cda10c7567d819f30738cfc0b47aadde65e
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-09-28 00:49:19 +0200

    fix: Add CssBaseline & remove normalize

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { createTheme, ThemeProvider } from "@mui/material";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
@@ -40,6 +40,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <RouterProvider router={router} />
       </ThemeProvider>
     </QueryClientProvider>
