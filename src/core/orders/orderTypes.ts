55c380f298bf20e1b52ed4773bb342bd597d56cc
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-09-29 02:40:36 +0200

    fix: Update order types

--- src/core/orders/orderTypes.ts
+++ src/core/orders/orderTypes.ts
@@ -1,10 +1,20 @@
+import { Control, FieldValues } from "react-hook-form";
+
+export type CurrencyType = {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  currentPrice?: number;
+};
+
 export type Order = {
   id: string;
   direction: "buy" | "sell";
-  cryptocurrency: string;
-  quantity: number;
-  value: number;
+  currency: CurrencyType;
   expirationDate: string;
+  quantity: number;
+  total: number;
 };
 
 export type OrderState = {
@@ -12,4 +22,8 @@ export type OrderState = {
   addOrder: (order: Order) => void;
   editOrder: (id: string, updatedOrder: Order) => void;
   deleteOrder: (id: string) => void;
+};
+
+export type PropsWithFormControl = {
+  control: Control<FieldValues, string>;
 };
