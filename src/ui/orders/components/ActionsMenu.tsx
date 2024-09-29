0e2473c69ddcf1f55257666f36ec5c09e98b3730
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-09-29 21:24:36 +0200

    feat: Add ActionsMenu component

---
+++ src/ui/orders/components/ActionsMenu.tsx
@@ -0,0 +1,67 @@
+import { MoreVert, Edit, Delete } from "@mui/icons-material";
+import { Menu, MenuItem, IconButton, Box } from "@mui/material";
+import { blue, red } from "@mui/material/colors";
+import { useState } from "react";
+import { COLORS } from "@/ui/constants/colors";
+
+export const ActionsMenu = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box>
+      <IconButton
+        onClick={handleClick}
+        size="small"
+        sx={{
+          "&:hover": {
+            backgroundColor: COLORS.hover.background,
+          },
+        }}
+      >
+        <MoreVert sx={{ color: COLORS.primary }} />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        slotProps={{ paper: { elevation: 1 } }}
+      >
+        <MenuItem
+          onClick={handleClose}
+          sx={{
+            paddingY: 1.5,
+            "&:hover": {
+              backgroundColor: COLORS.hover.background,
+              color: COLORS.hover.text,
+            },
+          }}
+        >
+          <Edit fontSize="small" sx={{ color: blue[500], marginRight: 1 }} />
+          Edit order
+        </MenuItem>
+        <MenuItem
+          onClick={handleClose}
+          sx={{
+            paddingY: 1.5,
+            "&:hover": {
+              backgroundColor: COLORS.hover.background,
+              color: COLORS.hover.text,
+            },
+          }}
+        >
+          <Delete fontSize="small" sx={{ color: red[500], marginRight: 1 }} />
+          Delete order
+        </MenuItem>
+      </Menu>
+    </Box>
+  );
+};
