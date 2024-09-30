7e409b777297396b3ce9b4e4316c1a908a34d3b5
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-09-30 20:40:14 +0200

    fix: Fix vite config TS error

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-// TODO: Even if we are not using the import defineConfigTest,
-// vite recognizes it and type the "test" object.
-// Try removing the below line
-import { defineConfig as defineConfigTest } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //@ts-expect-error TODO: TS Expect added because defineConfig dont accept a test object.
+  // I could have created a vitest.config.ts separately.
   test: {
     globals: true,
     environment: "jsdom",
