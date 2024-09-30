a4008a0aad488d78434f621ef7512e3090411c04
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-09-30 03:07:09 +0200

    feat: Abstracr OrderCard component to be reused by Delete Modal

---
+++ src/ui/orders/components/OrderCard.tsx
@@ -0,0 +1,120 @@
+import { AccessTime } from "@mui/icons-material";
+import { Box, Paper, Typography } from "@mui/material";
+import { blue, green } from "@mui/material/colors";
+import { ReactNode } from "react";
+import { CurrencyWithIcon } from "./CurrencyWithIcon";
+import { OrderType } from "@/core/orders/orderTypes";
+import { CustomTooltip } from "@/ui/components/CustomTooltip";
+import { COLORS } from "@/ui/constants/colors";
+import { formatUSDPrice } from "@/ui/utils/formatPrice";
+
+type OrderCardProps = {
+  order: OrderType | null;
+  actionsMenu?: ReactNode;
+};
+
+export const OrderCard = ({ order, actionsMenu }: OrderCardProps) => {
+  if (!order) return null;
+
+  return (
+    <Paper
+      key={order.id}
+      elevation={3}
+      sx={{
+        position: "relative",
+        paddingTop: 2.5,
+        paddingBottom: 1.5,
+        paddingX: 1.5,
+        bgcolor: COLORS.background2,
+        borderRadius: 2,
+        border: 1,
+        borderColor: order.direction === "buy" ? blue[500] : green[500],
+        marginBottom: 2,
+        display: "flex",
+        flexDirection: "column",
+        gap: 1.5,
+        width: "100%",
+      }}
+    >
+      {/* Direction Label on top */}
+      <Typography
+        variant="body1"
+        sx={{
+          position: "absolute",
+          top: -12,
+          left: 12,
+          backgroundColor: order.direction === "buy" ? blue[500] : green[500],
+          paddingX: 1,
+          color: COLORS.primary,
+          fontWeight: "bold",
+          fontSize: "0.9rem",
+          borderRadius: "100px",
+        }}
+      >
+        {order.direction.toUpperCase()}
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        {/* Currency */}
+        <CurrencyWithIcon
+          name={order.currency.name}
+          image={order.currency.image}
+          symbol={order.currency.symbol}
+        />
+
+        {/* Actions Menu */}
+        {actionsMenu}
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "end",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "start",
+            justifyContent: "space-between",
+            gap: 1,
+          }}
+        >
+          {/* Quantity */}
+          <Typography variant="body2" sx={{ fontSize: "0.9rem" }}>
+            Quantity: {order.quantity}
+          </Typography>
+
+          {/* Total in USD */}
+          <Typography
+            variant="body2"
+            sx={{ fontSize: "1rem", fontWeight: "bold" }}
+          >
+            USD {formatUSDPrice(order.total)}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: 1,
+            marginBottom: -1,
+          }}
+        >
+          <CustomTooltip title="Expiration date">
+            <AccessTime sx={{ color: "gray" }} />
+          </CustomTooltip>
+          <Typography variant="body2">{order.expirationDate}</Typography>
+        </Box>
+      </Box>
+    </Paper>
+  );
+};
