264c6bc93d23ee4c9d6113125b98e724e8fbd60d
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-09-30 19:43:19 +0200

    feat: Add testing library and configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+// TODO: Even if we are not using the import defineConfigTest,
+// vite recognizes it and type the "test" object.
+// Try removing the below line
+import { defineConfig as defineConfigTest } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/app/setupTest.ts"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
